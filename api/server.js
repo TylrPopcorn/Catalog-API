fb539d020dc2114dba169915552eff6c3cd72608
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-03-17 07:05:49 -0400

    ㅤㅤㅤㅤ

--- api/server.js
+++ api/server.js
@@ -3,7 +3,16 @@ const express = require("express");
     Dependencies:
 
     npm install express
+    npm i -D nodemon
 */
 const server = express();
 
 server.use(express.json()); //Allow the use of json request body fields
+
+server.get("/", (req, res) => {
+  res.send(`
+        <h1> Welcome to Node! <h1>
+    `);
+});
+//Exports
+module.exports = server;
--- index..js
+++ index..js
@@ -1,6 +1,6 @@
 console.log("node application practice");
 
-const server = require("./api/serve");
+const server = require("./api/server");
 
 server.listen(9000, () => {
   console.log("Server is running on port 9000");
