15fd6e1c196e3b7ca05abb03c384dcc9a54690d2
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2023-03-20 21:24:11 -0400

    ㅤㅤㅤ

--- api/hats/hats-model.js
+++ api/hats/hats-model.js
@@ -5,18 +5,13 @@ let hats = {};
 //------------------
 //Functions:---
 
-function getHats() {
-  axios
-    .get("https://catalog.roblox.com/v1/search/items/details")
-    .then((res) => {
-      for (let x of res.data.data) {
-        if (hats[x.name] == null) {
-          hats[x.name] = x;
-        }
-      }
-    })
-    .catch((err) => {});
-  return hats;
+async function getHats() {
+  const hats = await axios.get(
+    "https://catalog.roblox.com/v1/search/items/details"
+  );
+
+  console.log(hats.data.data);
+  return "Test";
 }
 
 //-------------
